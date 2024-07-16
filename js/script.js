147d83aec40539a956829562fdbc37b817987758
samilessons <samisecblog@gmail.com>
2024-07-16 18:57:28 +0400

    we added status for users which film count list will be more than 0 and we eddited answers with for loop

--- js/script.js
+++ js/script.js
@@ -7,15 +7,31 @@ const DB = {
   movies: {},
   actors: {},
   genrer: [],
+  status: null,
   privat: false
 };
 
-const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
-const b = +prompt("Ինչքա՞ն եք գնահատում այդ ֆիլմը");
-const c = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
-const d = +prompt("Ինչքա՞ն եք գնահատում այդ ֆիլմը");
+for (let i = 0; i < 2; i++) {
+  const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
+  const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
 
-DB.movies[a] = b;
-DB.movies[c] = d;
+  if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+    DB.movies[filmName] = filmRate;
+    console.log("Resolve");
+  } else {
+    console.log("Reject");
+    i--;
+  }
+}
+
+if (DB.count < 10) {
+  DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
+} else if (DB.count >= 10 && DB.count < 30) {
+  DB.status = "Դուք դասական ֆիլմ դիտող եք";
+} else if (DB.count >= 30) {
+  DB.status = "Դուք կինոման եք !"
+} else {
+  console.log("Տեղի է ունեցել խնդիր․");
+}
 
 console.log(DB);
