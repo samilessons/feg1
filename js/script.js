9bd24e9c94663891bc179b46e9feda36949fd071
samilessons <samisecblog@gmail.com>
2024-07-25 17:30:49 +0400

    On project we added functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
+let countOfFilms;
+
+init();
 
 const DB = {
   count: countOfFilms,
@@ -11,27 +13,45 @@ const DB = {
   privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-  const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
-  const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
+function init() {
+  countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
 
-  if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
-    DB.movies[filmName] = filmRate;
-    console.log("Resolve");
-  } else {
-    console.log("Reject");
-    i--;
+  while (countOfFilms == "" || countOfFilms == null || isNaN(countOfFilms)) {
+    countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
   }
 }
 
-if (DB.count < 10) {
-  DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
-} else if (DB.count >= 10 && DB.count < 30) {
-  DB.status = "Դուք դասական ֆիլմ դիտող եք";
-} else if (DB.count >= 30) {
-  DB.status = "Դուք կինոման եք !"
-} else {
-  console.log("Տեղի է ունեցել խնդիր․");
+function rememberAndCreateUserFilms() {
+  for (let i = 0; i < 2; i++) {
+    const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
+    const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
+
+    if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+      DB.movies[filmName] = filmRate;
+      console.log("Resolve");
+    } else {
+      console.log("Reject");
+      i--;
+    }
+  }
 }
 
-console.log(DB);
+function detectUserPersonalLevel() {
+  if (DB.count < 10) {
+    DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
+  } else if (DB.count >= 10 && DB.count < 30) {
+    DB.status = "Դուք դասական ֆիլմ դիտող եք";
+  } else if (DB.count >= 30) {
+    DB.status = "Դուք կինոման եք !"
+  } else {
+    console.log("Տեղի է ունեցել խնդիր․");
+  }
+}
+
+function checkDBPrivatStatus(state) {
+  state ? console.log("Sorry but DB is not visible") : console.log(DB);
+}
+
+// rememberAndCreateUserFilms();
+// detectUserPersonalLevel();
+checkDBPrivatStatus(DB.privat);
