53fd1fd898a444bfb6abf256f34ddbabfe0dc23c
samilessons <samisecblog@gmail.com>
2024-08-03 18:18:05 +0400

    changed file and all application we wraped in object, and used THIS

--- js/script.js
+++ js/script.js
@@ -1,64 +1,61 @@
 "use strict";
 
-let countOfFilms;
+// this = context = {}
 
-// init();
-
-const DB = {
-  count: countOfFilms,
+const app = {
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   status: null,
-  privat: false
+  privat: false,
+  init() {
+    this.count = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
+
+    while (this.count == "" || this.count == null || isNaN(this.count)) {
+      this.count = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
+    }
+  },
+  rememberAndCreateUserFilms() {
+    for (let i = 0; i < 2; i++) {
+      const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
+      const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
+
+      if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+        this.movies[filmName] = filmRate;
+        console.log("Resolve");
+      } else {
+        console.log("Reject");
+        i--;
+      }
+    }
+  },
+  detectUserPersonalLevel() {
+    if (this.count < 10) {
+      this.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
+    } else if (this.count >= 10 && this.count < 30) {
+      this.status = "Դուք դասական ֆիլմ դիտող եք";
+    } else if (this.count >= 30) {
+      this.status = "Դուք կինոման եք !"
+    } else {
+      console.log("Տեղի է ունեցել խնդիր․");
+    }
+  },
+  checkDBPrivatStatus(state) {
+    state ? console.log("Sorry but DB is not visible") : console.log(this);
+  },
+  useFavoriteGenres() {
+    for (let i = 0; i < 2; i++) {
+      this.genres.push(prompt(`Ձեր նախընտրելի ժանրը ${i + 1}`))
+    }
+  },
+  startApp() {
+    this.init();
+    this.rememberAndCreateUserFilms();
+    this.useFavoriteGenres();
+    this.detectUserPersonalLevel();
+    this.checkDBPrivatStatus(this.privat);
+  }
 };
 
-function init() {
-  countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
-
-  while (countOfFilms == "" || countOfFilms == null || isNaN(countOfFilms)) {
-    countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
-  }
-}
-
-function rememberAndCreateUserFilms() {
-  for (let i = 0; i < 2; i++) {
-    const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
-    const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
-
-    if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
-      DB.movies[filmName] = filmRate;
-      console.log("Resolve");
-    } else {
-      console.log("Reject");
-      i--;
-    }
-  }
-}
-
-function detectUserPersonalLevel() {
-  if (DB.count < 10) {
-    DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
-  } else if (DB.count >= 10 && DB.count < 30) {
-    DB.status = "Դուք դասական ֆիլմ դիտող եք";
-  } else if (DB.count >= 30) {
-    DB.status = "Դուք կինոման եք !"
-  } else {
-    console.log("Տեղի է ունեցել խնդիր․");
-  }
-}
-
-function checkDBPrivatStatus(state) {
-  state ? console.log("Sorry but DB is not visible") : console.log(DB);
-}
-
-function useFavoriteGenres() {
-  for (let i = 0; i < 2; i++) {
-    DB.genres.push(prompt(`Ձեր նախընտրելի ժանրը ${i + 1}`))
-  }
-}
-
-// rememberAndCreateUserFilms();
-// useFavoriteGenres();
-// detectUserPersonalLevel();
-// checkDBPrivatStatus(DB.privat);
+app.startApp();
